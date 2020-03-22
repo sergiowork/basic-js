ab62d2c8eb30d6883bbc48479054177317f60b4e
sergiowork <sergioworkit@gmail.com>
2020-03-22 18:35:41 +0200

    feat: update 'carbon-dating'

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity == 'string' && parseFloat(sampleActivity) < 0){
+  if (typeof(sampleActivity) === 'string' && 0 < parseFloat(sampleActivity) && parseFloat(sampleActivity) < 15){
     return Math.ceil(Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity))/(0.693/HALF_LIFE_PERIOD)); 
   } else {
     return false;
