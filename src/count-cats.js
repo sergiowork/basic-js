d2b62d11e85051385eecab7d0f7f2e4597f08aa7
sergiowork <sergioworkit@gmail.com>
2020-03-22 19:00:03 +0200

    feat: update function - countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,10 +1,12 @@
 module.exports = function countCats(matrix) {
+  let arrCats = matrix.join(',').split(',');
   let countCats = 0;
-  for(let i = 0; i < matrix.length; i++){
-    for(let j = 0; i < matrix[i].length; j++){
-      if(matrix[i][j] === "^^")
-      countCats++;
+
+  for (let i = 0; i < arrCats.length; i++) {
+    if (arrCats[i] == '^^') {
+       countCats++;
     }
-    return countCats;
   }
+  
+  return countCats;
 };
