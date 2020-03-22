8f8cb507752878821a96ca638936e32c8e51d029
sergiowork <sergioworkit@gmail.com>
2020-03-22 20:08:06 +0200

    feat: create function - createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  
+  if (!Array.isArray(members)) return false;
+
+  return members
+    .filter(item => typeof item === 'string')
+    .map(item => item.trim().split(' ')[0].toUpperCase()[0])
+    .sort()
+    .join('');
+  
 };
