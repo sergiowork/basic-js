56fe4ee9a448ddd1e924e00f86424088329723df
sergiowork <sergioworkit@gmail.com>
2020-03-22 18:32:15 +0200

    feat: create function - dateSample

---
+++ src/carbon-dating.js
@@ -0,0 +1,10 @@
+const MODERN_ACTIVITY= 15; 
+const HALF_LIFE_PERIOD= 5730;
+
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity == 'string' && parseFloat(sampleActivity) < 0){
+    return Math.ceil(Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity))/(0.693/HALF_LIFE_PERIOD)); 
+  } else {
+    return false;
+  }
+}; 
